20f9fe9e0e1be53eee7a093f9c30fa69937b584b
Andy Sapper <asapper@hotmail.com>
2017-06-02 22:26:40 -0600

    basic views implemented

--- registros/urls.py
+++ registros/urls.py
@@ -2,6 +2,21 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'registros'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # ex: /registros/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /registros/eventos/
+    url(r'^eventos/$', views.EventosView.as_view(), name='eventos'),
+    # ex: /registros/eventos/1/
+    url(r'^eventos/(?P<pk>[0-9]+)/$', views.EventoDetailView.as_view(), name='evento_detail'),
+    # ex: /registros/miembros/
+    url(r'^miembros/$', views.MiembrosView.as_view(), name='miembros'),
+    # ex: /registros/miembros/1/
+    url(r'^miembros/(?P<pk>[0-9]+)/$', views.MiembroDetailView.as_view(), name='miembro_detail'),
+    # ex: /registros/legados/
+    url(r'^legados/$', views.LegadosView.as_view(), name='legados'),
+    # ex: /registros/legados/1/
+    url(r'^legados/(?P<pk>[0-9]+)/$', views.LegadoDetailView.as_view(), name='legado_detail'),
 ]
--- registros/views.py
+++ registros/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateView
+
+from .models import Evento, Miembro, Legado
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the registros index.")
+class IndexView(TemplateView):
+    template_name = 'registros/index.html'
+
+
+class EventosView(ListView):
+    template_name = 'registros/eventos.html'
+    context_object_name = 'latest_eventos_list'
+
+    def get_queryset(self):
+        """Return all Eventos."""
+        return Evento.objects.all()
+
+
+class EventoDetailView(DetailView):
+    model = Evento
+    template_name = 'registros/evento_detail.html'
+
+
+class MiembrosView(ListView):
+    template_name = 'registros/miembros.html'
+    context_object_name = 'latest_miembros_list'
+
+    def get_queryset(self):
+        """Return all Miembros."""
+        return Miembro.objects.all()
+
+
+class MiembroDetailView(DetailView):
+    model = Miembro
+    template_name = 'registros/miembro_detail.html'
+
+
+class LegadosView(ListView):
+    template_name = 'registros/legados.html'
+    context_object_name = 'latest_legados_list'
+
+    def get_queryset(self):
+        """Return all Legados."""
+        return Legado.objects.all()
+
+
+class LegadoDetailView(DetailView):
+    model = Legado
+    template_name = 'registros/legado_detail.html'
