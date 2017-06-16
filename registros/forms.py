45cd8117005275244d28d446ad2e4a3cd1428120
Andy Sapper <asapper@hotmail.com>
2017-06-16 11:33:34 -0600

    implement basic password strength checking in registration

--- registros/forms.py
+++ registros/forms.py
@@ -16,6 +16,8 @@ class MiembroForm(forms.ModelForm):
         fields = ['nombre', 'apellido', 'correo', 'numero_de_legendario']
 
     def clean(self):
+        MIN_LENGTH = 8
+        # get cleaned data
         cleaned_data = super(MiembroForm, self).clean()
         contraseña1 = cleaned_data.get('contraseña1')
         contraseña2 = cleaned_data.get('contraseña2')
@@ -23,3 +25,11 @@ class MiembroForm(forms.ModelForm):
         if contraseña1 and contraseña2:
             if contraseña1 != contraseña2:
                 raise forms.ValidationError('Las contraseñas no coinciden.')
+        # check password strength
+        if len(contraseña1) < MIN_LENGTH:
+            raise forms.ValidationError(
+                    'La contraseña debe tener 8 caracteres o más.')
+        if all(char.isalpha() for char in contraseña1):
+            raise forms.ValidationError(
+                    ('La contraseña debe contener una combinación de \
+                     letras y al menos un número (a-z, A-Z, 0-9).'))
